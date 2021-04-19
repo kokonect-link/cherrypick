c8e5cf48929909ec9ccf3a989231444d841e3871
NoriDev <NoriDev@users.noreply.github.com>
2021-04-20 01:48:58 +0900

    tweak ui

--- src/client/ui/default.vue
+++ src/client/ui/default.vue
@@ -338,13 +338,13 @@ export default defineComponent({
 	> .post {
 			position: fixed;
 			z-index: 1000;
-			bottom: 64px;
+			bottom: 64px 0 calc(env(safe-area-inset-bottom) + 94px);
 			width: 55px;
 			height: 55px;
 			border-radius: 100%;
 			box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);
 			font-size: 22px;
-			right: 32px;
+			right: 15px;
 		}
 
 	> .buttons {
