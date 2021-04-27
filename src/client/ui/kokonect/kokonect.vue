76098668a5eb4459dc53721f826d529a6392f1f0
NoriDev <NoriDev@users.noreply.github.com>
2021-04-28 02:31:21 +0900

    Modify the note-taking button so that it is only visible in the timeline

--- src/client/ui/kokonect/kokonect.vue
+++ src/client/ui/kokonect/kokonect.vue
@@ -25,7 +25,7 @@
 		</div>
 	</div>
 
-	<div class="floatbtn" v-if="isMobile">
+	<div class="floatbtn" v-if="isMobile && !(withBack && canBack)">
 		<button v-if="$i" class="post _buttonPrimary" @click="post()"><i class="fas fa-pencil-alt"/></button>
   </div>
 
