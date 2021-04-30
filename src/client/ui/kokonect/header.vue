7978cdd89f4f7b17e00c3314be27c2bfcbace652
NoriDev <m1nthing2322@gmail.com>
2021-05-01 01:44:13 +0900

    fix

--- src/client/ui/kokonect/header.vue
+++ src/client/ui/kokonect/header.vue
@@ -24,8 +24,8 @@
 		</div>
 	</template>
 	<transition :name="$store.state.animation ? 'header' : ''" mode="out-in" appear>
-		<button class="_button back" v-if="withBack && canBack && !($route.name === 'notifications' || $route.name === 'messaging')" @click.stop="back()" v-tooltip="$ts.goBack"><i class="fas fa-chevron-left"></i></button>
-		<button class="_button back" v-else-if="withBack && canBack" @click.stop="back()" v-tooltip="$ts.goBack"><i class="fas fa-chevron-left"></i></button>
+		<button class="_button back" v-if="withBack && canBack && isMobile && !($route.name === 'notifications' || $route.name === 'messaging')" @click.stop="back()" v-tooltip="$ts.goBack"><i class="fas fa-chevron-left"></i></button>
+		<button class="_button back" v-else-if="withBack && canBack && !isMobile" @click.stop="back()" v-tooltip="$ts.goBack"><i class="fas fa-chevron-left"></i></button>
 	</transition>
 </div>
 </template>
