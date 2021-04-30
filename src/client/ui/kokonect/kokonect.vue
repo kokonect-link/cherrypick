ce26c500d40aa25895e104eccd1ebf44c25fa6ce
NoriDev <m1nthing2322@gmail.com>
2021-05-01 00:20:22 +0900

    fix

--- src/client/ui/kokonect/kokonect.vue
+++ src/client/ui/kokonect/kokonect.vue
@@ -35,7 +35,7 @@
 		<button class="button home _button" @click="$route.name === 'index' ? top() : $router.push('/')"><i class="fas fa-home"></i></button>
 		<button class="button search _button" @click="search"><i class="fas fa-search"/></button>
 		<button class="button notifications _button" @click="$router.replace('/my/notifications')"><i class="fas fa-bell"></i><span v-if="$i.hasUnreadNotification" class="indicator"><i class="fas fa-circle"></i></span></button>
-		<button class="button tab _button" @click="$router.replace('/my/messaging')""><i class="fas fa-comments"></i><span v-if="$i.hasUnreadMessagingMessage" class="indicator"><i class="fas fa-circle"></i></span></button>
+		<button class="button tab _button" @click="$router.replace('/my/messaging')"><i class="fas fa-comments"></i><span v-if="$i.hasUnreadMessagingMessage" class="indicator"><i class="fas fa-circle"></i></span></button>
 		<button class="button widget _button" @click="widgetsShowing = true"><i class="fas fa-layer-group"></i></button>
 		<!-- <button class="button post _button" @click="post"><i class="fas fa-pencil-alt"></i></button> -->
 	</div>
