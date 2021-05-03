bdf859237c336c5a526140323502b0ff6f4e464c
NoriDev <m1nthing2322@gmail.com>
2021-05-03 22:33:06 +0900

    fix

--- src/client/pages/user/index.vue
+++ src/client/pages/user/index.vue
@@ -112,7 +112,7 @@
 							<span v-if="user.isAdmin" :title="$ts.isAdmin" style="color: var(--badge);"><i class="fas fa-bookmark"></i></span>
 							<span v-if="!user.isAdmin && user.isModerator" :title="$ts.isModerator" style="color: var(--badge);"><i class="far fa-bookmark"></i></span>
 							<span v-if="user.isLocked" :title="$ts.isLocked"><i class="fas fa-lock"></i></span>
-							<span v-if="user.isPatron" :title="$ts.isPatron" style="color: var(--patron)"><i class="fas fa-star"></i></span>
+							<span v-if="user.isPatron" :title="$ts.isPatron" style="color: var(--patron);"><i class="fas fa-star"></i></span>
 							<span v-if="user.isBot" :title="$ts.isBot"><i class="fas fa-robot"></i></span>
 						</div>
 					</div>
