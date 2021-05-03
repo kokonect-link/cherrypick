8ae71733bb3c42281239099cf69c749e3cf7f0a3
NoriDev <m1nthing2322@gmail.com>
2021-05-03 22:46:56 +0900

    change patron icon

--- src/client/components/note-header.vue
+++ src/client/components/note-header.vue
@@ -7,7 +7,7 @@
 	<span class="username"><MkAcct :user="note.user"/></span>
 	<span class="admin" v-if="note.user.isAdmin"><i class="fas fa-bookmark"></i></span>
 	<span class="moderator" v-if="!note.user.isAdmin && note.user.isModerator"><i class="far fa-bookmark"></i></span>
-	<span class="patron" v-if="note.user.isPatron"><i class="fas fa-star"></i></span>
+	<span class="patron" v-if="note.user.isPatron"><i class="fas fa-heart"></i></span>
 	<div class="info">
 		<span class="mobile" v-if="note.viaMobile"><i class="fas fa-mobile-alt"></i></span>
 		<MkA class="created-at" :to="notePage(note)">
--- src/client/pages/instance/users.vue
+++ src/client/pages/instance/users.vue
@@ -49,7 +49,7 @@
 						<span class="acct">@{{ acct(user) }}</span>
 						<span class="staff" v-if="user.isAdmin"><i class="fas fa-bookmark"></i></span>
 						<span class="staff" v-if="user.isModerator"><i class="far fa-bookmark"></i></span>
-						<span class="staff" v-if="user.isPatron"><i class="fas fa-star"></i></span>
+						<span class="staff" v-if="user.isPatron"><i class="fas fa-heart"></i></span>
 						<span class="punished" v-if="user.isSilenced"><i class="fas fa-microphone-slash"></i></span>
 						<span class="punished" v-if="user.isSuspended"><i class="fas fa-snowflake"></i></span>
 					</header>
