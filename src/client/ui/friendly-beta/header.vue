486d599b49d5f0fea61348d0716ad5d60916e9db
NoriDev <m1nthing2322@gmail.com>
2021-06-07 23:59:46 +0900

    Friendly UI: UI 개선

--- src/client/ui/friendly-beta/header.vue
+++ src/client/ui/friendly-beta/header.vue
@@ -265,6 +265,7 @@ export default defineComponent({
 			line-height: 0;
 			background: var(--pick);
 			margin-top: 10px;
+			box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);
 
 			&:hover {
 				background: var(--pickLighten);
