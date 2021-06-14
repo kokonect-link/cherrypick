b0fc16e4b1965544315991ff1beab367ffa6ebf7
NoriDev <m1nthing2322@gmail.com>
2021-06-14 16:03:02 +0900

    UI 개선: 버튼 그림자 변경

--- src/client/pages/timeline.vue
+++ src/client/pages/timeline.vue
@@ -431,7 +431,7 @@ export default defineComponent({
 			margin: var(--margin) auto 0 auto;
 			padding: 8px 16px;
 			border-radius: 32px;
-			box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);
+			box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 3px 3px 0 rgba(0, 0, 0, 0.14), 0 1px 3px 0 rgba(0, 0, 0, 0.12);
 
 			> i {
 				margin-right: 5px;
