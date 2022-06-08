1b3fb2ae551cb25ff22956bf00484bf2b4a239b9
Ryu jongheon <lapy@lapy.link>
2022-06-08 09:12:22 +0900

    Fix SmtpCharLimit migration on downgrade may fail
    smtpHost/smtpUser/smtpPass 가 128보다 긴 상황에서 다운그레이드할 때, 마이그레이션이 실패하거나 설정이 깨질 위험이 있기 때문에 아예 터치하지 않는 걸로

--- packages/backend/migration/1654589815249-increase-smtp-char-limit.js
+++ packages/backend/migration/1654589815249-increase-smtp-char-limit.js
@@ -8,8 +8,10 @@ export class increaseSmtpCharLimit1654589815249 {
     }
 
     async down(queryRunner) {
-			await queryRunner.query(`ALTER TABLE "meta" ALTER COLUMN "smtpHost" TYPE character varying(128)`);
-			await queryRunner.query(`ALTER TABLE "meta" ALTER COLUMN "smtpUser" TYPE character varying(128)`);
-			await queryRunner.query(`ALTER TABLE "meta" ALTER COLUMN "smtpPass" TYPE character varying(128)`);
+			// It's safe to keep extended character limit, rather than shrinking with risk of breaking settings
+			
+			// await queryRunner.query(`ALTER TABLE "meta" ALTER COLUMN "smtpHost" TYPE character varying(128)`);
+			// await queryRunner.query(`ALTER TABLE "meta" ALTER COLUMN "smtpUser" TYPE character varying(128)`);
+			// await queryRunner.query(`ALTER TABLE "meta" ALTER COLUMN "smtpPass" TYPE character varying(128)`);
     }
 }
