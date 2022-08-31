47ee60b0f0029bb84078469931bca15d22f99249
NoriDev <m1nthing2322@gmail.com>
2022-09-01 00:03:50 +0900

    Fix floating button position error, change color

--- packages/client/src/ui/friendly.vue
+++ packages/client/src/ui/friendly.vue
@@ -262,7 +262,7 @@ const wallpaper = localStorage.getItem('wallpaper') != null;
 		display: block;
 		position: fixed;
 		z-index: 1000;
-		bottom: 70px;
+		bottom: calc(70px + env(safe-area-inset-bottom));
 		left: 10px;
 		width: $float-button-size;
 		height: $float-button-size;
@@ -282,14 +282,18 @@ const wallpaper = localStorage.getItem('wallpaper') != null;
 		display: block;
 		position: fixed;
 		z-index: 1000;
-		bottom: 70px;
+		bottom: calc(70px + env(safe-area-inset-bottom));
 		right: 0;
 		width: $float-button-size;
 		height: $float-button-size;
 		box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);
 		font-size: 22px;
-		background: var(--panel);
+		background: var(--accent);
 		opacity: 0.7;
+
+		i {
+			color: white;
+		}
 	}
 
 	> .widgetButton {
