75a0a7abfe8c3d68228c20aae6aa1549251b9bc2
NoriDev <m1nthing2322@gmail.com>
2022-09-03 23:48:29 +0900

    enhance: Change button gradient color

--- packages/client/src/components/ui/button.vue
+++ packages/client/src/components/ui/button.vue
@@ -189,11 +189,11 @@ export default defineComponent({
 		background: linear-gradient(90deg, var(--buttonGradateA), var(--buttonGradateB));
 
 		&:not(:disabled):hover {
-			background: linear-gradient(90deg, var(--X8), var(--X8));
+			background: linear-gradient(90deg, var(--cherryX8), var(--pickX8));
 		}
 
 		&:not(:disabled):active {
-			background: linear-gradient(90deg, var(--X8), var(--X8));
+			background: linear-gradient(90deg, var(--cherryX8), var(--pickX8));
 		}
 	}
 
