68c37707415f6e4f61007d76ea9824ba6988b99b
ltlapy <lapy@lapy.link>
2023-02-19 01:01:30 +0900

    backend: Insert moderation log when copying emoji

--- packages/backend/src/server/api/endpoints/admin/emoji/copy.ts
+++ packages/backend/src/server/api/endpoints/admin/emoji/copy.ts
@@ -86,6 +86,10 @@ export default class extends Endpoint<typeof meta, typeof paramDef> {
 				driveFile,
 			});
 
+			this.moderationLogService.insertModerationLog(me, 'copyEmoji', {
+				emojiId: emoji.id,
+			});
+
 			return {
 				id: copied.id,
 			};
