af9712f006dc912c4e72e0023c2a8ba5d09ca67f
NoriDev <m1nthing2322@gmail.com>
2023-06-01 20:08:30 +0900

    feat(client): ネットワークウィジェットの単位に秒追加

--- packages/frontend/src/filters/bytes-net-sizes.ts
+++ packages/frontend/src/filters/bytes-net-sizes.ts
@@ -1,6 +1,6 @@
 export default (v) => {
 	if (v == null) return '?';
-	const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+	const sizes = ['B/s', 'KB/s', 'MB/s', 'GB/s', 'TB/s'];
 	if (v === 0) return sizes[0];
 	const i = Math.floor(Math.log(v) / Math.log(1024));
 	return sizes[i];
