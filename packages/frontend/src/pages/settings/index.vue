4895e05f6d2ac71029d6d2dc38e5134812f8bc51
NoriDev <m1nthing2322@gmail.com>
2023-06-15 01:39:28 +0900

    設定ページ順序の変更

--- packages/frontend/src/pages/settings/index.vue
+++ packages/frontend/src/pages/settings/index.vue
@@ -170,15 +170,15 @@ const menuDef = computed(() => [{
 		to: '/settings/migration',
 		active: currentPage?.route.name === 'migration',
 	}, {
-		icon: 'ti ti-dots',
-		text: i18n.ts.other,
-		to: '/settings/other',
-		active: currentPage?.route.name === 'other',
-	}, {
 		icon: 'ti ti-bulb-filled',
 		text: 'CherryPick',
 		to: '/settings/cherrypick',
 		active: currentPage?.route.name === 'cherrypick',
+	}, {
+		icon: 'ti ti-dots',
+		text: i18n.ts.other,
+		to: '/settings/other',
+		active: currentPage?.route.name === 'other',
 	}],
 }, {
 	items: [{
