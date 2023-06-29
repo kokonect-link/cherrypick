1de49672e896831f672d55b9ec34777f4bd9b1ff
mlatus <wqseleven@gmail.com>
2023-06-29 11:57:56 +0800

    fix(backend): publish notes announced by relay
    Publish notes announced by relays instead of creating renotes.
    Some relays (e.g. aode-relay, buzzrelay) deliver notes using Announce
    activity, which will result in a lot of renotes in the timeline, usually
    this is not desired.

--- packages/backend/src/core/activitypub/ApInboxService.ts
+++ packages/backend/src/core/activitypub/ApInboxService.ts
@@ -29,6 +29,7 @@ import type { UsersRepository, NotesRepository, FollowingsRepository, MessagingM
 import { bindThis } from '@/decorators.js';
 import type { MiRemoteUser } from '@/models/User.js';
 import { getApHrefNullable, getApId, getApIds, getApType, isAccept, isActor, isAdd, isAnnounce, isBlock, isCollection, isCollectionOrOrderedCollection, isCreate, isDelete, isFlag, isFollow, isLike, isMove, isPost, isRead, isReject, isRemove, isTombstone, isUndo, isUpdate, validActor, validPost } from './type.js';
+import { GlobalEventService } from '@/core/GlobalEventService.js';
 import { ApNoteService } from './models/ApNoteService.js';
 import { ApLoggerService } from './ApLoggerService.js';
 import { ApDbResolverService } from './ApDbResolverService.js';
@@ -87,6 +88,7 @@ export class ApInboxService {
 		private apQuestionService: ApQuestionService,
 		private queueService: QueueService,
 		private messagingService: MessagingService,
+		private globalEventService: GlobalEventService,
 	) {
 		this.logger = this.apLoggerService.logger;
 	}
@@ -301,11 +303,14 @@ export class ApInboxService {
 		const meta = await this.metaService.fetch();
 		if (this.utilityService.isBlockedHost(meta.blockedHosts, this.utilityService.extractDbHost(uri))) return;
 
+		const relays = await this.relayService.getAcceptedRelays();
+		const fromRelay = !!actor.inbox && relays.map(r => r.inbox).includes(actor.inbox);
+
 		const unlock = await this.appLockService.getApLock(uri);
 
 		try {
 			// 既に同じURIを持つものが登録されていないかチェック
-			const exist = await this.apNoteService.fetchNote(uri);
+			const exist = await this.apNoteService.fetchNote(fromRelay ? targetUri : uri);
 			if (exist) {
 				return;
 			}
@@ -330,6 +335,12 @@ export class ApInboxService {
 
 			if (!await this.noteEntityService.isVisibleForMe(renote, actor.id)) {
 				this.logger.warn('skip: invalid actor for this activity');
+				return;
+			}
+
+			if (fromRelay) {
+				const noteObj = await this.noteEntityService.pack(renote);
+				this.globalEventService.publishNotesStream(noteObj);
 				return;
 			}
 
