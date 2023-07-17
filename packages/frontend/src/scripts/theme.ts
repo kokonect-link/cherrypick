2ad07eb2a5702d4a1cbc47faec799fee43e9ee76
caipira113 <caipira@libnare.net>
2023-07-17 16:37:54 +0900

    Add Byeolvit-Theme https://github.com/Luminon/Byeolvit-Theme/tree/404d1e159328723e49f3aa848c99a3b1d41b01a9

--- packages/frontend/src/scripts/theme.ts
+++ packages/frontend/src/scripts/theme.ts
@@ -31,6 +31,7 @@ export const getBuiltinThemes = () => Promise.all(
 		'l-sushi',
 		'l-u0',
 		'l-birdsite',
+		'l-byeolvit-polaris',
 
 		'd-cherrypick',
 		'd-dark',
@@ -45,6 +46,7 @@ export const getBuiltinThemes = () => Promise.all(
 		'd-u0',
 		'd-qdon',
 		'd-birdsite',
+		'd-byeolvit-noctiluca',
 	].map(name => import(`../themes/${name}.json5`).then(({ default: _default }): Theme => _default)),
 );
 
