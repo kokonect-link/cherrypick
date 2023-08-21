358662795e7833f470b7567fb9f9e2a6142bd920
NoriDev <m1nthing2322@gmail.com>
2023-08-21 17:03:42 +0900

    デフォルトのダークテーマを「Rosé Pine Moon」に変更

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -519,7 +519,7 @@ interface Watcher {
  */
 import { miLocalStorage } from './local-storage';
 import lightTheme from '@/themes/l-cherrypick.json5';
-import darkTheme from '@/themes/d-cherrypick.json5';
+import darkTheme from '@/themes/d-rosepinemoon.json5';
 import { Note, UserDetailed, Page } from 'cherrypick-js/built/entities';
 
 export class ColdDeviceStorage {
