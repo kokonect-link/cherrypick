97d59c0541249bc3b8801c9b4bec6f026230503f
NoriDev <m1nthing2322@gmail.com>
2023-08-22 17:34:33 +0900

    fix lint

--- packages/frontend/src/components/MkAnnouncementDialog.vue
+++ packages/frontend/src/components/MkAnnouncementDialog.vue
@@ -23,7 +23,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 
 <script lang="ts" setup>
 import { onMounted, shallowRef } from 'vue';
-import * as misskey from 'misskey-js';
+import * as misskey from 'cherrypick-js';
 import * as os from '@/os';
 import MkModal from '@/components/MkModal.vue';
 import MkButton from '@/components/MkButton.vue';
--- packages/frontend/src/components/MkUserAnnouncementEditDialog.vue
+++ packages/frontend/src/components/MkUserAnnouncementEditDialog.vue
@@ -51,7 +51,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 
 <script lang="ts" setup>
 import { } from 'vue';
-import * as misskey from 'misskey-js';
+import * as misskey from 'cherrypick-js';
 import MkModalWindow from '@/components/MkModalWindow.vue';
 import MkButton from '@/components/MkButton.vue';
 import MkInput from '@/components/MkInput.vue';
