1f59d42a75a1ecc9cc646963c53a2db736c74a95
NoriDev <m1nthing2322@gmail.com>
2023-09-19 16:41:44 +0900

    fix store.ts

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -188,7 +188,7 @@ export const defaultStore = markRaw(new Storage('base', {
 	tl: {
 		where: 'deviceAccount',
 		default: {
-			src: 'home' as 'home' | 'local' | 'social' | 'global' | `list:${string}`,
+			src: 'home' as 'home' | 'local' | 'media' | 'social' | 'cat' | 'global' | `list:${string}`,
 			userList: null as Misskey.entities.UserList | null,
 		},
 	},
