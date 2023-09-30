207bd642c3097d1f51adc322c51399a396a317ba
NoriDev <m1nthing2322@gmail.com>
2023-10-01 02:47:06 +0900

    chore

--- packages/backend/migration/1696044626209-noteEditHistory.js
+++ packages/backend/migration/1696044626209-noteEditHistory.js
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: syuilo and other misskey contributors
+ * SPDX-FileCopyrightText: syuilo and other misskey, cherrypick contributors
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
