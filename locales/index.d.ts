d8038e48c85609c181d05a104b8a0086f17da00f
NoriDev <m1nthing2322@gmail.com>
2023-10-25 16:26:40 +0900

    Update index.d.ts

--- locales/index.d.ts
+++ locales/index.d.ts
@@ -370,7 +370,7 @@ export interface Locale {
     "createFolder": string;
     "renameFolder": string;
     "deleteFolder": string;
-    "Folder": string;
+    "folder": string;
     "addFile": string;
     "emptyDrive": string;
     "emptyFolder": string;
