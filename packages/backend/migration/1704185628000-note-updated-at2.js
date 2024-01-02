bdf96da9dab171003f9fbfc3622a013103ce6f76
NoriDev <m1nthing2322@gmail.com>
2024-01-02 19:40:53 +0900

    fix 6df51bff

--- packages/backend/migration/1704185628000-note-updated-at2.js
+++ packages/backend/migration/1704185628000-note-updated-at2.js
@@ -2,8 +2,16 @@ export class NoteUpdatedAt1704185628000 {
     name = 'NoteUpdatedAt1704185628000'
 
     async up(queryRunner) {
-				await queryRunner.query(`ALTER TABLE "note" DROP COLUMN "updatedAt"`);
-        await queryRunner.query(`ALTER TABLE "note" ADD "updatedAt" TIMESTAMP WITH TIME ZONE`);
+				await queryRunner.query(`SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'note' AND column_name = 'updatedAt'`)
+					.then(updatedAt => {
+						if (updatedAt.length > 0) {
+							return [];
+						} else {
+							return [
+								queryRunner.query(`ALTER TABLE "note" ADD "updatedAt" TIMESTAMP WITH TIME ZONE`)
+							];
+						}
+					})
     }
 
     async down(queryRunner) {
