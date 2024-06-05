4eeea51f7ac7503cbc6f466005e7c067be66ab3e
NoriDev <m1nthing2322@gmail.com>
2024-06-05 12:34:27 +0900

    fix a4c682c3

--- packages/backend/src/core/AvatarDecorationService.ts
+++ packages/backend/src/core/AvatarDecorationService.ts
@@ -45,6 +45,7 @@ export class AvatarDecorationService implements OnApplicationShutdown {
 		private httpRequestService: HttpRequestService,
 	) {
 		this.cache = new MemorySingleCache<MiAvatarDecoration[]>(1000 * 60 * 30);
+		this.cacheWithRemote = new MemorySingleCache<MiAvatarDecoration[]>(1000 * 60 * 30);
 
 		this.redisForSub.on('message', this.onMessage);
 	}
