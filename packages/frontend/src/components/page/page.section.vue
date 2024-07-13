1314034f5f90c618ad38bcabb6f3d2b1f5bdc382
Esurio <rekishinomori@gmail.com>
2024-07-13 08:18:49 +0000

    pageの可読性を向上

--- packages/frontend/src/components/page/page.section.vue
+++ packages/frontend/src/components/page/page.section.vue
@@ -4,14 +4,14 @@ SPDX-License-Identifier: AGPL-3.0-only
 -->
 
 <template>
-<section>
+<section :class="$style.section">
 	<component
 		:is="'h' + h"
-		:class="{
-			'h2': h === 2,
-			'h3': h === 3,
-			'h4': h === 4,
-		}"
+		:class="
+			h === 2 ? $style.h2 :
+			h === 3 ? $style.h3 :
+			h === 4 ? $style.h4 : ''
+		"
 	>
 		{{ block.title }}
 	</component>
@@ -37,17 +37,26 @@ defineProps<{
 
 <style lang="scss" module>
 .h2 {
-	font-size: 1.35em;
+	font-size: 1.6em;
 	margin: 0 0 0.5em 0;
+	border-bottom: 1px solid var(--accentLighten);
+	padding: 0.1em;
 }
 
 .h3 {
-	font-size: 1em;
+	font-size: 1.25em;
 	margin: 0 0 0.5em 0;
+	border-left: 3px solid var(--accentLighten);
+	border-bottom: 1px solid var(--accentLighten);
+	padding: 0.05em 0.5em 0.15em;
 }
 
 .h4 {
 	font-size: 1em;
 	margin: 0 0 0.5em 0;
+}
+
+.section {
+	margin-bottom: 1em;
 }
 </style>
