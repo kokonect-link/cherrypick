08f1b111385041a61465cbde1f8fc96d381d2852
NoriDev <m1nthing2322@gmail.com>
2024-10-03 10:58:24 +0900

    chore

--- packages/backend/src/server/web/boot.embed.js
+++ packages/backend/src/server/web/boot.embed.js
@@ -61,13 +61,13 @@
 		}
 		const meta = await metaRes.json();
 		const v = meta.version;
-		const vMisskey = meta.basedMisskeyVersion;
+		const basedMisskeyVersion = meta.basedMisskeyVersion;
 		if (v == null) {
 			renderError('META_FETCH_V');
 			return;
 		}
 
-		if (vMisskey == null) {
+		if (basedMisskeyVersion == null) {
 			renderError('META_FETCH_BASEDMISSKEY_V');
 			return;
 		}
