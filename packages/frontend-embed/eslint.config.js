86ab74e4f1b88ea8e78f7c9813bede8d6feaebd3
NoriDev <m1nthing2322@gmail.com>
2024-10-06 20:10:55 +0900

    chore

--- packages/frontend-embed/eslint.config.js
+++ packages/frontend-embed/eslint.config.js
@@ -24,10 +24,11 @@ export default [
 				require: false,
 				__dirname: false,
 
-				// Misskey
+				// CherryPick
 				_DEV_: false,
 				_LANGS_: false,
 				_VERSION_: false,
+				_BASEDMISSKEYVERSION_: false,
 				_ENV_: false,
 				_PERF_PREFIX_: false,
 				_DATA_TRANSFER_DRIVE_FILE_: false,
