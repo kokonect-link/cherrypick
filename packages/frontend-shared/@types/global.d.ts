a55c1f509e3dac016c58d46a4e0b5ff6c0e5d089
NoriDev <m1nthing2322@gmail.com>
2025-10-26 05:20:15 +0900

    fix b7b1ac3f

--- packages/frontend-shared/@types/global.d.ts
+++ packages/frontend-shared/@types/global.d.ts
@@ -9,6 +9,7 @@ type FIXME = any;
 declare const _LANGS_: string[][];
 declare const _VERSION_: string;
 declare const _BASEDMISSKEYVERSION_: string;
+declare const _GIT_HASH_: string;
 declare const _ENV_: string;
 declare const _DEV_: boolean;
 declare const _PERF_PREFIX_: string;
