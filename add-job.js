855cb6ebbfc029466ff5798e4a763752dca1da70
NoriDev <m1nthing2322@gmail.com>
2025-10-29 00:17:17 +0900

    fix 533d2d05

--- add-job.js
+++
@@ -1,6 +0,0 @@
-const Bull = require('bullmq');
-const q = new Bull.Queue('system', {connection: {host: 'redis', port: 6379}});
-q.add('autoDeleteNotes', {}).then(() => {
-  console.log('Job added');
-  process.exit(0);
-});
